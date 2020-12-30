5519d2b5d17e779edafb24b71feeb56b29a3e4bc
Ali Ahmad <callalishykh@gmail.com>
2020-12-30 12:59:17 +0500

    added counter and stock button

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -3,13 +3,31 @@ import LayoutWrapper from '../components/utility/layoutWrapper';
 import Papersheet from '../components/utility/papersheet';
 import { FullColumn } from '../components/utility/rowColumn';
 import IntlMessages from '../components/utility/intlMessages';
-
+import image from '../images/honey.jpg';
+import Hit from '../containers/Page/products/products'
+let hit={
+	'price':100,
+	'rating' : 2,
+	'image':image
+};
 export default () => (
 	<LayoutWrapper>
 		<FullColumn>
 			<Papersheet title={<IntlMessages id="sidebar.dashboard" />}>
 				Mate Dashboard
 			</Papersheet>
+			<div className="algoliaMainWrapper">
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			<Hit hit={hit}/>
+			</div>
 		</FullColumn>
+		{/* <RecipeReviewCard title={"hahh"} img={image} date={"2010/12/12"} message={"hasdfhdshfhsdfhshdfsjdhfbhsbcvdjsbhvffhjdsfbvhfvbhjbfvhjdfbvhjbdfvhjbfvjhbdfvhjdfvbhdfbh	"}/> */}
+
 	</LayoutWrapper>
 );
---
+++ src/containers/Page/products/products.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Highlight, Snippet } from 'react-instantsearch/dom';
+// import ecommerceActions from '../../../redux/ecommerce/actions';
+import {
+  GridListViewWrapper,
+  Button,
+  CartIcon,
+  Rate,
+} from '../../../components/algolia/algoliaComponent.style';
+// import Button from '@material-ui/core/Button';
+import Countdown from '../products/countdown'
+// const { addToCart, changeViewTopbarCart } = ecommerceActions;
+
+class Hit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      addCartLoading: false,
+    };
+  }
+  render() {
+    const currentDate = new Date();
+    const year = (currentDate.getMonth() === 11 && currentDate.getDate() > 23) ? currentDate.getFullYear() + 1 : currentDate.getFullYear();
+  
+    const { hit } = this.props;
+    // const className =
+    //   this.props.view === 'gridView'
+    //     ? 'algoliaGrid GridView'
+    //     : 'algoliaGrid ListView';
+    // let addedTocart = false;
+    // this.props.productQuantity.forEach(product => {
+    //   if (product.objectID === hit.objectID) {
+    //     addedTocart = true;
+    //   }
+    // });
+    return (
+      <GridListViewWrapper >
+        <div className="alGridImage">
+          <img alt="#" src={hit.image} />
+          {/* {!addedTocart ? ( */}
+         {/* <Button */}
+        {/* //       onClick={() => {
+                this.setState({ addCartLoading: true });
+                const update = () => {
+                  this.props.addToCart(hit);
+                  this.setState({ addCartLoading: false });
+                };
+                setTimeout(update, 1500);
+              }}
+            >
+              <CartIcon>shopping_cart</CartIcon>
+              Add to cart
+            </Button>
+          ) : (
+            <Button
+              onClick={() => this.props.changeViewTopbarCart(true)}
+              type="button"
+            >
+              View Cart
+            </Button>
+          )}*/}
+        </div> 
+        <div className="alGridContents">
+        <div class="alGridName">
+            <span class="ais-Highlight">
+            <span class="ais-Highlight-nonHighlighted">Amazon - Fire TV Stick</span>
+            </span>
+        </div>
+
+          <div className="alGridPriceRating">
+            <span className="alGridPrice">${hit.price}</span>
+
+            <div className="alGridRating">
+              <Rate
+                disabled
+                starCount={6}
+                value={hit.rating}
+                name="algoliaRating"
+              />
+        <Countdown date={`${year}-12-24T00:00:00`} />
+                     <Button variant="contained" size="small" color="primary" style={{color: "red",width:"30px",height:"30px"}}>
+        Primary
+      </Button>
+            </div>
+     
+          </div>
+
+          <div className="alGridDescription">
+            {/* <Snippet attribute="description" hit={hit} /> */}
+          </div>
+        </div>
+      </GridListViewWrapper>
+    );
+  }
+}
+function mapStateToProps(state) {
+  const { productQuantity } = state.Ecommerce;
+  return {
+    productQuantity,
+  };
+}
+export default(Hit);
