9949984081d6e977ff3c4095bac4593df612d03b
Amaaz <amaaz@pop-os.localdomain>
2021-01-05 22:40:36 +0500

    test

--- src/containers/Page/listOfProducts/listOfProducts.js
+++ src/containers/Page/listOfProducts/listOfProducts.js
@@ -1,31 +1,15 @@
-import React from "react";
-import LayoutWrapper from "../../../components/utility/layoutWrapper";
-import Papersheet from "../../../components/utility/papersheet";
-import { FullColumn } from "../../../components/utility/rowColumn";
-import hit from "./hit";
-import Hit from "./products";
+import React from 'react';
+import LayoutWrapper from '../../../components/utility/layoutWrapper';
+import Papersheet from '../../../components/utility/papersheet';
+import { FullColumn } from '../../../components/utility/rowColumn';
 
-// let dummy = [];
-// for (var i = 0; i < 10; ++i) {
-//   dummy.push(
-//     new Hit(
-//       {
-//         image:
-//           "https://png.pngtree.com/element_our/sm/20180323/sm_5ab4a26e8d73b.jpg",
-//       },
-//       { price: 1200 }
-//     )
-//   );
-// }
-
-// let pp = (element) => {
-//   return <Hit hit={element}></Hit>;
-// };
 
 export default () => (
-  <LayoutWrapper>
-    <FullColumn>
-      <Papersheet title="List of Products"></Papersheet>
-    </FullColumn>
-  </LayoutWrapper>
+	<LayoutWrapper>
+		<FullColumn>
+			<Papersheet title="List of Products">
+				Implement list of products here
+			</Papersheet>
+		</FullColumn>
+	</LayoutWrapper>
 );
