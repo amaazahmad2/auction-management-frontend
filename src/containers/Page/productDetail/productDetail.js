388a2ef0ffefed5623b3a96eabd03e12cbf4a0e8
Afaq Ahmad <afaq11072001@gmail.com>
2021-01-12 03:34:23 +0500

    ProductDetails page started

--- src/containers/Page/productDetail/productDetail.js
+++ src/containers/Page/productDetail/productDetail.js
@@ -2,13 +2,34 @@ import React, { Component } from "react";
 import LayoutWrapper from "../../../components/utility/layoutWrapper";
 import Papersheet from "../../../components/utility/papersheet";
 import { Column, FullColumn } from "../../../components/utility/rowColumn";
+import firebase from "firebase";
+import FirebaseHelper from "../../../helpers/firebase/index";
+import Main from "./showDetails.js"
 
 export default class ProductDetail extends Component {
+
+    constructor(props, context) {
+        super();
+        this.state = {
+            product: null,
+        };
+
+        this.state.product = null;
+        const listRef = firebase.database().ref("data/product/0");
+        listRef.on("value", (snapshot) => {
+            this.setState({product: snapshot.val()});
+            //console.log("snapshot: ", this.state.product);
+        });
+    }
+
     render() {
+
         return (
             <LayoutWrapper>
                 <FullColumn>
-                    <Papersheet title="Product Detail"></Papersheet>
+                    <Papersheet title="Check amuz dummdumm">
+                        <Main props = {this.state.product}/>
+                    </Papersheet>
                 </FullColumn>
             </LayoutWrapper>
         );
---
+++ src/containers/Page/productDetail/showDetails.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import ImageGrid from './imageGrid'
+
+const useStyles = makeStyles((theme) => ({
+  mainFeaturedPost: {
+    position: 'relative',
+    backgroundColor: theme.palette.grey[800],
+    color: theme.palette.common.white,
+    marginBottom: theme.spacing(4),
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+  },
+  overlay: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    backgroundColor: 'rgba(0,0,0,.3)',
+  },
+  mainFeaturedPostContent: {
+    position: 'relative',
+    padding: theme.spacing(3),
+    [theme.breakpoints.up('md')]: {
+      padding: theme.spacing(6),
+      paddingRight: 0,
+    },
+  },
+}));
+
+export default function Main(props) {
+  const classes = useStyles();
+  const post = props.props;
+  console.log(post);
+
+  return (
+      <div>
+    <Paper className={classes.mainFeaturedPost} style={{ backgroundImage: `url(${"https://picsum.photos/300/200"})` }}>
+      {/* Increase the priority of the hero background image */}
+      {<img style={{ display: 'none' }} src={post && post.image}/>}
+      <div className={classes.overlay} />
+      <Grid container>
+        <Grid item md={6}>
+          <div className={classes.mainFeaturedPostContent}>
+            <Typography component="h1" variant="h3" color="inherit" gutterBottom>
+              Name
+            </Typography>
+            <Typography variant="h5" color="inherit" paragraph>
+              Description
+            </Typography>
+            <Link variant="subtitle1" color="white" date>
+              11-07-21
+            </Link>
+          </div>
+        </Grid>
+      </Grid>
+    </Paper>
+    <ImageGrid />
+    </div>
+  );
+}
