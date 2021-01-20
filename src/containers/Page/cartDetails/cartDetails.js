d327b8a62fc3a10ef8019237369073ba217da752
Afaq Ahmad <afaq11072001@gmail.com>
2021-01-21 02:28:53 +0500

    Initialized cartDetails Page

--- src/containers/Page/cartDetails/cartDetails.js
+++ src/containers/Page/cartDetails/cartDetails.js
@@ -2,6 +2,14 @@ import React from "react";
 import LayoutWrapper from "../../../components/utility/layoutWrapper";
 import Papersheet from "../../../components/utility/papersheet";
 import { Column, FullColumn } from "../../../components/utility/rowColumn";
+import Orders from "./cartList";
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 import { Row, Col, Container } from "reactstrap";
 
@@ -44,7 +52,10 @@ class CartDetails extends React.Component {
     return (
       <LayoutWrapper>
         <FullColumn>
-          <Papersheet title="Cart"></Papersheet>
+          <Papersheet title="Cart">
+            <Orders />
+            
+          </Papersheet>
         </FullColumn>
       </LayoutWrapper>
     );
