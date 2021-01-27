0cc58811e604e9d6d99f66f3a85d6d7bf42d26cc
Amaaz <amaaz@pop-os.localdomain>
2021-01-28 02:15:48 +0500

    uuid passed as props to create product

--- src/containers/Page/editProduct/editProduct.js
+++ src/containers/Page/editProduct/editProduct.js
@@ -4,12 +4,17 @@ import LayoutWrapper from "../../../components/utility/layoutWrapper";
 import { FullColumn } from "../../../components/utility/rowColumn";
 import Papersheet from "../../../components/utility/papersheet";
 
-export default () => (
+export default function EditProduct(props){
+
+    const product_uuid = props.match.params.key; 
+
+    return(
     <LayoutWrapper>
       <FullColumn>
         <Papersheet title="Edit product">
-          <ProductCreate isEditPage={true} />
+          <ProductCreate isEditPage={true} productUUID={product_uuid}/>
         </Papersheet>
       </FullColumn>
     </LayoutWrapper>
-  );
+    )
+}
